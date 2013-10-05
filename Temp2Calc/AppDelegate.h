3d9ed91bc28cfce36b153df8a74b094bed95f6ae
John <john@admins-MacBook-Pro.local>
2013-10-05 10:57:50 -0700

    Initial Commit

---
+++ Temp2Calc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Temp2Calc
+//
+//  Created by John on 10/5/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
